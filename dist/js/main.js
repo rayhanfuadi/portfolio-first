34a89a20c704e353883f3796e59cd789e9dc84bd
rayhanfuadi <oke.rayhan1@gmail.com>
2024-05-15 16:05:47 +0700

    update darkMode dan perbaikan fitur

--- dist/js/main.js
+++ dist/js/main.js
@@ -2,11 +2,18 @@
 window.onscroll = function(){
     const header = document.querySelector('header');
     const fixNav = header.offsetTop;
+    const toTop = document.querySelector('#toTop');
+
+
 
     if(window.pageYOffset > fixNav){
         header.classList.add('navbar-fixed');
+        toTop.classList.remove('hidden');
+        toTop.classList.add('flex');
     } else {
         header.classList.remove('navbar-fixed');
+        toTop.classList.remove('flex');
+        toTop.classList.add('hidden');
     }
 }
 
@@ -17,4 +24,34 @@ const navMenu = document.querySelector('#nav-menu');
 hamburger.addEventListener('click', function(){
     hamburger.classList.toggle('hamburger-active');
     navMenu.classList.toggle('hidden');
+});
+
+// klik diluar hamburgernya
+window.addEventListener('click', function(e){
+    if(e.target != navMenu && e.target != hamburger){
+        hamburger.classList.remove('hamburger-active');
+        navMenu.classList.add('hidden');
+    }
 })
+
+// dark mode toggle
+const darkToggle = document.querySelector('#dark-toggle')
+const html = document.querySelector('html');
+
+darkToggle.addEventListener('click', function(){
+    if(darkToggle.checked){
+        html.classList.add('dark');
+        localStorage.theme = ('dark')
+    } else {
+        html.classList.remove('dark');
+        localStorage.theme = ('light');
+    }
+})
+
+
+// pindahkan posisi toggle sesuai mode
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+  darkToggle.checked = true;
+} else {
+  darkToggle.checked = false;
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['index.html'],
+  darkMode: 'class',
   theme: {
     container: {
       center: true,
@@ -10,8 +11,10 @@ module.exports = {
       colors:{
         primary: '#F2CF30',
         dark: '#3A3132',
+        dark2: '#443B3C',
         soft: '#F2EDD7',
         regular: '#777777',
+        light: '#999999',
       },
       screens: {
         '2xl': '1320px'
